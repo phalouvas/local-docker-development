897854b2ec7f36c5eae6ccbb1b37bba2d707ba13
Panayiotis Halouvas <phalouvas@kainotomo.com>
2019-08-02 08:32:30 +0300

    added authentication microservice

--- docker/include/mariadb/initialize.sql
+++ docker/include/mariadb/initialize.sql
@@ -3,9 +3,12 @@ CREATE DATABASE IF NOT EXISTS smsto /*\!40100 DEFAULT CHARACTER SET utf8 */;
 CREATE USER IF NOT EXISTS smsto@'%' IDENTIFIED BY 'smsto';
 GRANT ALL PRIVILEGES ON smsto.* TO 'smsto'@'%';
 
-# Create SMSTO DB
+# Create Messages Microservice DB
 CREATE DATABASE IF NOT EXISTS msms /*\!40100 DEFAULT CHARACTER SET utf8 */;
-CREATE USER IF NOT EXISTS smsto@'%' IDENTIFIED BY 'smsto';
+GRANT ALL PRIVILEGES ON smsto.* TO 'smsto'@'%';
+
+# Create Authentication Microservice DB
+CREATE DATABASE IF NOT EXISTS msas /*\!40100 DEFAULT CHARACTER SET utf8 */;
 GRANT ALL PRIVILEGES ON smsto.* TO 'smsto'@'%';
 
 # Create EmailVerifier DB
