6c3c9d50633208953e53449d201878f90f52e8db
Panayiotis Halouvas <phalouvas@kainotomo.com>
2019-09-30 11:27:54 +0300

    added mock server

--- docker/include/mariadb/initialize.sql
+++ docker/include/mariadb/initialize.sql
@@ -19,7 +19,7 @@ CREATE USER IF NOT EXISTS emailverifier@'%' IDENTIFIED BY 'emailverifier';
 GRANT ALL PRIVILEGES ON emailverifier.* TO 'emailverifier'@'%';
 
 # Create Mock Server DB
-CREATE DATABASE IF NOT EXISTS msms /*\!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS mockserver /*\!40100 DEFAULT CHARACTER SET utf8 */;
 CREATE USER IF NOT EXISTS mockserver@'%' IDENTIFIED BY 'mockserver';
 GRANT ALL PRIVILEGES ON mockserver.* TO 'mockserver'@'%';
 
